2130fec14620f252385a6f36cada4f375f1cf114
Rory James <84684031+rjames187@users.noreply.github.com>
2024-04-03 16:29:19 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -14,3 +14,7 @@ def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
   assert fix_phone_num("5554429876") == '(555) 442 9876'
   assert fix_phone_num("3216543333") == '(321) 654 3333'
+
+def test_other_formats():
+  assert fix_phone_num("555-442-98761") == '(555) 442 9876'
+  assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
